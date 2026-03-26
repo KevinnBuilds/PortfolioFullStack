91c3aefd174d65211a5c23156e2a4e34f760438f
KevinMakeMoney <kevins.dvp@gmail.com>
2026-03-26 23:57:34 +0100

    push per vercel

--- src/components/layout/ContactBbar.tsx
+++ src/components/layout/ContactBbar.tsx
@@ -5,7 +5,7 @@ import { Send, Mail, Linkedin, Github, CheckCircle } from "lucide-react";
 export const ContactBar = () => {
   return (
     <div
-      className={`relative glass flex w-full items-center justify-between gap-3 rounded-2xl px-4 py-2 transition-all duration-300 md:min-h-[90vh] md:w-auto md:flex-col md:items-center md:justify-start md:px-3 md:py-5 md:gap-5 `}
+      className={`relative glass flex w- items-center justify-between gap-3 rounded-2xl px-4 py-2 transition-all duration-300 md:min-h-[90vh] md:w-auto md:flex-col md:items-center md:justify-start md:px-3 md:py-5 md:gap-5 `}
     ></div>
   );
 };
